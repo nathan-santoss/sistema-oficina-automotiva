4ddc293f669ee7b69ea2d9226468c012aeb25dd2
nathan-santoss <nathan.oliver07@hotmail.com>
2025-12-13 10:50:05 -0300

    :books: att

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { app, BrowserWindow, ipcMain, Menu, dialog } from "electron";
 import path from 'path'
 import { fileURLToPath } from "url";
-import {fs} from 'fs'
+import fs from 'fs'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const preload = path.join(__dirname, 'preload.js')
-const home_html = path.join(__dirname, '../app/')
+const dataBase = path.join(__dirname, './database.json')
+const home_html = path.join(__dirname, '../app/home/home.html')
+
+let clientes = []
 
 let win = null
 const criarJanela = () => {
@@ -21,10 +24,35 @@ const criarJanela = () => {
     })
     win.maximize()
     win.removeMenu()
-    win.loadFile()
-    win.webContents.openDevTools()
+    win.loadFile(home_html)
+    // win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
+    uptade_clients(dataBase, clientes)
     criarJanela()
 })
+
+//função para salvar e atualizar novos clientes
+
+const salvarDados = (destino, array) => {
+    try{
+        const fileJSON = JSON.stringify(array, null, 2)
+        fs.writeFileSync(destino, fileJSON, 'utf-8')
+    }catch(e){
+        console.error(`(Linha 33 - main.js) FALHA em salvar novos dados: ${e}`)
+    }
+}
+const uptade_clients = (localSaves, array) => {
+    try{
+        const fileString = fs.readFileSync(localSaves, 'utf-8')
+        const fileJSON = JSON.parse(fileString)
+        if(fileJSON.length === 0 || fileJSON === ''){
+            console.log('Sem clientes cadastrados');
+            return
+        }
+        array = fileJSON
+    }catch(e){
+        console.error('Erro ao atualizar lista de clientes')
+    }
+}
